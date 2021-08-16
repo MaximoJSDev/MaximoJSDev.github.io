37589d61266937e34c716a983bb7a441bce65e3a
Maximo-4ever <choripapa04@gmail.com>
2021-08-15 21:14:53 -0500

    Prueba Responsive

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
+let oldBtn;
 const $btn = document.querySelectorAll(".nav__menu__a");
 const $nav = document.querySelector(".nav");
 const $audio = (document.getElementById("audio").volume = 0.03);
-let oldBtn;
-
 const $submit = document.querySelector(".contact__form");
-const $btnMailTo = document.querySelector("#mailto");
+const $btnMailTo = document.getElementById("mailto");
 
 window.addEventListener("scroll", function () {
   $nav.classList.toggle("sticky", window.scrollY > 1);
@@ -23,7 +22,6 @@ $submit.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   const form = new FormData(this);
-  console.log(form.get("name"));
   $btnMailTo.setAttribute(
     "href",
     `mailto:maximiliano06cm@gmail.com?subject=${form.get("name")}${form.get("email")}&body=${form.get("message")}`
