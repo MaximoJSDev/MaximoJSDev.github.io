dc9bab92d423c448c150797a552b64da7647561b
Maximo-4ever <choripapa04@gmail.com>
2021-09-09 19:34:35 -0500

    diseño de mini ventana

--- scripts.js
+++ scripts.js
@@ -7,11 +7,12 @@ const backModal = document.querySelector(".bakckground-modal");
 const $modal = document.querySelector(".modal");
 const $btnModal = document.querySelector(".modal__btn");
 const $submit = document.querySelector(".contact__form");
-const $btnMailTo = document.getElementById("mailto");
 window.addEventListener("scroll", function () {
   $nav.classList.toggle("sticky", window.scrollY > 1);
 });
-$btnResponsive.addEventListener("click", () => $menu.classList.toggle("menuResponsive"));
+$btnResponsive.addEventListener("click", () =>
+  $menu.classList.toggle("menuResponsive")
+);
 $menu.addEventListener("click", (e) => {
   if (e.target && e.target.className == "nav__menu__a") {
     e.target.classList.add("active");
@@ -36,18 +37,19 @@ const removeModal = () => {
   $modal.classList.remove("active-modal");
   setTimeout(() => backModal.classList.remove("back-modal-active"), 280);
 };
-// $submit.addEventListener("submit", handleSubmit);
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const form = new FormData(this);
-//   $btnMailTo.setAttribute(
-//     "href",
-//     `mailto:maximilianocm06@gmail.com?subject=${form.get("name")}  |  ${form.get(
-//       "email"
-//     )}&body=${form.get("message")}`
-//   );
-//   $btnMailTo.click();
-//   inputName = document.getElementById("name").value.reset();
-//   inputEmail = document.getElementById("email").value.reset();
-//   inputMessage = document.getElementById("message").value.reset();
-// }
+$submit.addEventListener("submit", handleSubmit);
+async function handleSubmit(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      "Accept": "aplication/json",
+    },
+  });
+  if (response.ok) {
+    const contactWindow = document.querySelector(".contact__window").classList.add("active");
+    this.reset();
+  }
+}
